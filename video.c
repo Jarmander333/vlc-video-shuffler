6cc8a7609f7160a448a326d26f0c22ea61ad9a2e
Jarmander333 <57240500+Jarmander333@users.noreply.github.com>
2021-07-06 14:46:07 -0500

    Add updates
    Added everything from the "To-do List" to the program, except an automated python3 setup program

--- video.c
+++ video.c
@@ -3,34 +3,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <unistd.h>
+#include <string.h>
+ 
 int main(void) {
     for(;;) {
     	system("clear");
 
-	    srand(time(0));
+	srand(time(0));
 
     	int r = rand() % 6;
-
-    	printf("%d", r);
+    	char vid1[32] = "vlc [your_video.mp4]";
+    	char vid2[32] = "vlc [your_video.mp4]";
+    	char vid3[32] = "vlc [your_video.mp4]";
+    	char vid4[32] = "vlc [your_video.mp4]";
+    	char vid5[32] = "vlc [your_video.mp4]"; 
+    	char vid6[32] = "vlc [your_video.mp4]";
+    	char exit;
 
 	    if(r == 0) {
-	    	system("vlc video01.mp4");
+	    	printf("\nNow Playing:'%.*s'\n\n", 32, vid1 + 4);
+	    	sleep(5);
+	    	system(vid1);
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } else if(r == 1) {
-	    	system("vlc video02.mp4");
+	    	printf("\nNow Playing:'%.*s'\n\n", 32, vid2 + 4);
+	    	sleep(5);
+	    	system(vid2);
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } else if(r == 2) {
-	    	system("vlc video03.mp4");
+	    	printf("\nNow Playing:'%.*s'\n\n", 32, vid3 + 4);
+	    	sleep(5);
+	    	system(vid3);
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } else if(r == 3) {
-	    	system("vlc video04.mp4");
+	    	printf("\nNow Playing:'%.*s'\n\n", 32, vid4 + 4);
+	    	sleep(5);
+	    	system(vid4);
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } else if(r == 4) {
-	    	system("vlc video05.mp4");
+	    	printf("\nNow Playing:'%.*s'\n\n", 32, vid5 + 4);
+	    	sleep(5);
+	    	system(vid5);
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } else if(r == 5) {
-	    	system("vlc video06.mp4");
+	    	printf("\nNow Playing:'%.*s'\n\n", 32, vid6 + 4);
+	    	sleep(5);
+	    	system(vid6);
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } else {
 	    	printf("No Video Present");
+	    	exit = getchar();
+	    	if(exit == 0x0A) {
+	    		break;
+	    	}
 	    } 
 	}
 
     return 0;
 }
-
